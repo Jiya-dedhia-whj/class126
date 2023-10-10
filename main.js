b2a8634cae0ee37ad818b5b2eeea13a23fe5cb0d
Jiya-dedhia-whj <94034939+Jiya-dedhia-whj@users.noreply.github.com>
2023-10-10 14:25:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,6 +12,31 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("leftWristX = " + leftWristX + " , leftWristY = " + leftWristY);
+        console.log("rightWristX = " + rightWristX + " , rightWristY = " + rightWristY);
+    }
 }
 
 function draw()
@@ -22,4 +47,11 @@ function draw()
 function play()
 {
     song.play();
+    song.setVolume(0.1);
 }
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
