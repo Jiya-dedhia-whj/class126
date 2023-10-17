b7c4d6ba98e3c7f06b215c4dca9797aafb830d3e
Jiya-dedhia-whj <94034939+Jiya-dedhia-whj@users.noreply.github.com>
2023-10-17 14:22:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -60,12 +60,43 @@ function draw()
         document.getElementById("volume").innerHTML = "volume = " +volume;
         song.setVolume(volume);
     }
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        if(rightWristY > 0 && rightWristY <= 100)
+        {
+           document.getElementById("speed").innerHTML = "Speed = 0.5x";
+           song.rate(0.5);
+        }
+        else if(rightWristY > 100 && rightWristY <= 200)
+        {
+           document.getElementById("speed").innerHTML = "Speed = 1x";
+           song.rate(1);
+        }        
+        else if(rightWristY > 200 && rightWristY <= 300)
+        {
+           document.getElementById("speed").innerHTML = "Speed = 1.5x";
+           song.rate(1.5);
+        }        
+        else if(rightWristY > 300 && rightWristY <= 400)
+        {
+           document.getElementById("speed").innerHTML = "Speed = 2x";
+           song.rate(2);
+        }        
+        else if(rightWristY > 400)
+        {
+           document.getElementById("speed").innerHTML = "Speed = 2.5x";
+           song.rate(2.5);
+        }
+    }
 }
 
 function play()
 {
     song.play();
-    song.setVolume(0.1);
+    song.setVolume(0.5);
+    song.rate(1);
 }
 
 leftWristX = 0;
